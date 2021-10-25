3a9cd4e8a88e08dbed41e42a77e019b661b36105
changguojielucid <changguo.ji@elucidbio.com>
2021-10-25 16:10:19 -0400

    some updates

--- EVWorkClient.h
+++ EVWorkClient.h
@@ -1,17 +1,30 @@
 #pragma once
 
+#include "EVFirst.h"
 #include <QMap>
 #include "EVWorkItemManager.h"
+#include "EVWebSocketClient.h"
 
-class EVWorkClient : public  QObject
+#ifdef __EV_SERVER_USE_CWEBSOCKET__
+  class EVWorkClient : public  EVWebSocketClient
+#else
+  class EVWorkClient : public  QObject
+#endif //!__EV_SERVER_USE_CWEBSOCKET__
 {
   Q_OBJECT
 public:
-  EVWorkClient();
+  EVWorkClient(QObject *parent = nullptr);
   ~EVWorkClient();
 
+  void Receive(const QByteArray& message); // data received from remote client
+  void Receive(const QString& message);    // data received from remote client
+  void Receive(const QJsonObject& json);    // data received from remote client
 
-  bool CreateWorkItem(const std::string& seriesPath);
+  void Send(const QByteArray& message);    // send to remote client  
+  void Send(const QString& message);       // send to remote client
+  void Send(const QJsonObject& json);      // send to remote client
+
+  EVWorkItem* CreateWorkItem(const std::string& seriesPath);
   EVWorkItemManager* GetWorkItemManger() { return m_pWorkItemManager; }
 
 public slots:
